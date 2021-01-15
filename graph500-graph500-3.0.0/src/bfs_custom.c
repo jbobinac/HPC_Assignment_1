97adcf700f23ed30f7dd871c698a32e33e86fa95
Bernhard <bha@mission-embedded.com>
2021-01-15 16:19:56 +0100

    Working serial version.

--- graph500-graph500-3.0.0/src/bfs_custom.c
+++ graph500-graph500-3.0.0/src/bfs_custom.c
@@ -30,14 +30,63 @@ void make_graph_data_structure(const tuple_graph* const tg) {
 	visited_size = (g.nlocalverts + ulong_bits - 1) / ulong_bits;
 	visited = xmalloc(visited_size*sizeof(unsigned long));
 	//user code to allocate other buffers for bfs
+	rowstarts=g.rowstarts;
 }
 
 //user should provide this function which would be called several times to do kernel 2: breadth first search
 //pred[] should be root for root, -1 for unrechable vertices
 //prior to calling run_bfs pred is set to -1 by calling clean_pred
 void run_bfs(int64_t root, int64_t* pred) {
-	pred_glob=pred;
+
 	//user code to do bfs
+	
+  int64_t nvisited = 0;
+  
+  // queue stuff
+	int* q1 = xmalloc(g.nlocalverts*sizeof(int));
+	int* q2 = xmalloc(g.nlocalverts*sizeof(int));
+  int q1c, q2c;
+ 
+  // alloc iterators
+  unsigned int i, j;
+ 
+	pred_glob=pred;
+
+	CLEAN_VISITED();
+	for(i=0;i<g.nlocalverts;i++) q1[i]=0,q2[i]=0;
+
+	nvisited=1;
+  pred[VERTEX_LOCAL(root)]=root;
+	SET_VISITED(root);
+	q1[0]=VERTEX_LOCAL(root);
+	q1c = 1; 
+
+	// While there are vertices in current level
+	while(q1c != 0) {
+
+		q2c=0;
+
+		//for all vertices in current level send visit AMs to all neighbours
+		for( i = 0; i < q1c; i++ ) {
+		  //printf("Checking neighbours for %d!\n", q1[i]);
+			for( j = rowstarts[q1[i]]; j < rowstarts[q1[i]+1]; j++ ) {
+			  //printf("Found neighbour %d \n", COLUMN(j));
+				if (!TEST_VISITED(COLUMN(j))) {
+				  //printf("Not visitid\n");
+					SET_VISITED(COLUMN(j));
+					pred[COLUMN(j)] = VERTEX_LOCAL(q1[i]);
+					q2[q2c++] = COLUMN(j);
+				}
+			}
+		}
+
+    // swap queues
+		q1c = q2c; int *tmp=q1; q1=q2; q2=tmp;
+		nvisited += q1c;
+
+	}
+
+  free(q1); free(q2);
 }
 
 //we need edge count to calculate teps. Validation will check if this count is correct
